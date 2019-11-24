a448c12dd97a255e28968ee4c378e300c004061f
Parth Kheni <kheni.parth@gmail.com>
2019-11-23 20:29:21 -0500

    remove comments

--- admin/class-aoda-atag-admin.php
+++ admin/class-aoda-atag-admin.php
@@ -60,21 +60,9 @@ class Aoda_Atag_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_styles() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Aoda_Atag_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Aoda_Atag_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/aoda-atag-admin.css', array(), $this->version, 'all' );
-
+		wp_register_style( 'bootstrap-min',  plugin_dir_url( __FILE__ ) .'/css/bootstrap.min.css', array(), null, 'all' );
+		wp_enqueue_style( 'bootstrap-min' );
 	}
 
 	/**
@@ -83,21 +71,11 @@ class Aoda_Atag_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Aoda_Atag_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Aoda_Atag_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/aoda-atag-admin.js', array( 'jquery' ), $this->version, false );
-
+		wp_register_script('bootstrap-min', plugin_dir_url( __FILE__ ) . 'js/bootstrap.min.js');
+		wp_enqueue_script( 'bootstrap-min');
+		wp_register_script('jquery-steps', plugin_dir_url( __FILE__ ) . 'js/jquery.steps.js');
+		wp_enqueue_script( 'jquery-steps');
 	}
 
 
@@ -108,17 +86,7 @@ class Aoda_Atag_Admin {
 	 */
 
 	public function add_plugin_admin_menu() {
-
-		/*
-		* Add a settings page for this plugin to the Settings menu.
-		*
-		* NOTE:  Alternative menu locations are available via WordPress administration menu functions.
-		*
-		*        Administration Menus: http://codex.wordpress.org/Administration_Menus
-		*
-		*/
-		add_options_page( 'AODA Anchor Tag', 'AODA Anchor Tag', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page')
-		);
+		add_options_page( 'AODA Anchor Tag', 'AODA Anchor Tag', 'manage_options', $this->plugin_name, array($this, 'display_plugin_setup_page'));
 	}
 
 	/**
@@ -126,16 +94,11 @@ class Aoda_Atag_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-
 	public function add_action_links( $links ) {
-		/*
-		*  Documentation : https://codex.wordpress.org/Plugin_API/Filter_Reference/plugin_action_links_(plugin_file_name)
-		*/
-	$settings_link = array(
-		'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
-	);
-	return array_merge(  $settings_link, $links );
-
+		$settings_link = array(
+			'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+		);
+		return array_merge(  $settings_link, $links );
 	}
 
 	/**
@@ -143,16 +106,13 @@ class Aoda_Atag_Admin {
 	 *
 	 * @since    1.0.0
 	 */
-
 	public function display_plugin_setup_page() {
 		include_once( 'partials/aoda-atag-admin-display.php' );
 	}
 
-	public function validate($input) {
-		// All checkboxes inputs        
+	public function validate($input) {    
 		$valid = array();
 	
-		//Cleanup
 		$valid['switch'] = (isset($input['switch']) && !empty($input['switch'])) ? 1 : 0;
 		$valid['domains'] = esc_textarea($input['domains']);
 		$valid['element'] = esc_textarea($input['element']);
@@ -164,5 +124,4 @@ class Aoda_Atag_Admin {
 	 public function options_update() {
 		register_setting($this->plugin_name, $this->plugin_name, array($this, 'validate'));
 	 }
-
 }
